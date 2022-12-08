610a38e33af1eedbe985dd2345dd2fbac1d0d617
ulise <ulise@192.168.56.1>
2022-12-08 03:48:35 -0600

    Carga inicial

---
+++ src/main/java/com/mycompany/juegosocket/JuegoSocket.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.juegosocket;
+
+/**
+ *
+ * @author ulise
+ */
+public class JuegoSocket {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
