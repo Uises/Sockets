bc11d025c4c765d3ebc9167d55ca9ab52601bff4
ulise <ulise@192.168.56.1>
2022-12-08 04:24:46 -0600

    Actualizacion

--- src/main/java/com/mycompany/juegosocket/JuegoSocket.java
+++ src/main/java/com/mycompany/juegosocket/JuegoSocket.java
@@ -1,16 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.juegosocket;
 
-/**
- *
- * @author ulise
- */
+import java.net.Socket;
+
 public class JuegoSocket {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+      Socket sc = new Socket();
     }
 }
